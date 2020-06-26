9169ee6908bdb0333a7710601d5624ddcb9e3af0
Matthew Leite <mleite@aimconsulting.com>
2020-06-26 16:27:56 -0700

    First commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+// import express from 'express';
+// const test = true;
+// if (test) {
+//   // test
+// }
+
+// BROKEN JS FOR TESTING LINTING
+"use strict";
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import express from 'express';
+// const test = true;
+// if (test) {
+//   // test
+// }
+
+// BROKEN JS FOR TESTING LINTING
